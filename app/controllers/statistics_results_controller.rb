3870bd97609a37e97d71377a66c6f0eb4ff347c0
Gabriel <gabrielrambo_@hotmail.com>
2013-06-14 00:25:16 -0300

    Correcao no google chart

--- app/controllers/statistics_results_controller.rb
+++ app/controllers/statistics_results_controller.rb
@@ -8,6 +8,7 @@ class StatisticsResultsController < ApplicationController
   def index
     
     @statistics_results = {}
+    @count_results = 0
     
     #Busca todos os usuários
     user_all = User.all
@@ -16,13 +17,10 @@ class StatisticsResultsController < ApplicationController
     day_of_match = Match.where('date >= ?', DateTime.now).order(:date).select(:date).first
     matches_of_day = Match.where('date >= ? AND date <= ?', day_of_match.date, day_of_match.date.end_of_day).order(:date)
     
-    matches_of_day.each do |match|
-    
+    matches_of_day.each do |match| 
       #Popula o hash com a selação e o total de palpites   
       @statistics_results = { match.team1.name => 0, match.team2.name => 0 } 
-      
       user_all.each do |user|
-      
         #Existirá apenas um palpite do usuário a cada partida, por isso o first
         result = Result.where('user_id = ? AND match_id = ?', user.id, match.id).first
         
@@ -33,11 +31,26 @@ class StatisticsResultsController < ApplicationController
             elsif (result.result1 < result.result2)
               @statistics_results[match.team2.name] += 1 
             end
+            @count_results += 1
           end
         end
       end
     end
-     
+    
+      
+    
+    count = @statistics_results.count
+    
+    #Deserializa objeto para o formato a qual o google chart interpreta
+    @google_chart = ""
+    @statistics_results.each do |sr|
+      @google_chart = @google_chart + "['" + sr[0].to_s + "' , " + sr[1].to_s + "]"
+      count -= 1
+      unless (count == 0)
+        @google_chart = @google_chart + ", "
+      end
+    end
+  
   end
 
 end
