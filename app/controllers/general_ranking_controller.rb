7192074ffceb1f9dcf568e2d03d5355334e59253
Gabriel <gabrielrambo_@hotmail.com>
2013-06-16 18:41:26 -0300

    Melhorias

--- app/controllers/general_ranking_controller.rb
+++ app/controllers/general_ranking_controller.rb
@@ -20,9 +20,13 @@ class GeneralRankingController < ApplicationController
     results.each do |result|
       @general_ranking.each do |gr|
         if gr[:user] == result.user
-          pointing = Pointing.new
-          pointing.point = gr[:pointing].point + Pointing.get_a_score(result).point
-          gr[:pointing] = pointing
+          #Validação: é possível que o usuário tenha acessado o Quero Palpitar,
+          #porém ele não palpitou, dessa forma result1 e result2 estarão nulos
+          unless result.result1.nil? && result.result2.nil?
+            pointing = Pointing.new
+            pointing.point = gr[:pointing].point + Pointing.get_a_score(result).point
+            gr[:pointing] = pointing
+          end
         end
       end
     end
