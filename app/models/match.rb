e8425e3a0a5ea110868c92b06e19ef47698bbd50
Gabriel <gabrielrambo_@hotmail.com>
2013-06-19 21:50:30 -0300

    Adicionado resource Match: Agora é possível alteração dos resultado dos jogos pelo painel administrativo

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,7 @@
 class Match < ActiveRecord::Base
   belongs_to :team1, :class_name => 'Team', :foreign_key => 'team1_id'
   belongs_to :team2, :class_name => 'Team', :foreign_key => 'team2_id'
-  attr_accessible :date, :final_result1, :final_result2, :group, :locale
+  attr_accessible :date, :final_result1, :final_result2, :group, :locale, :team1_id, :team2_id
    
   def self.next_match_of_current_time
     self.where('date >= ?', Time.zone.now).order(:date).first
